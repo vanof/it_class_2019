effa96bc327ee30cf8354f33a29d51beb5b17106
vanof <58184758+vanof@users.noreply.github.com>
2020-04-16 11:02:01 +0300

    добавил материалы по работе с функциями для работы со структурами

---
+++ Examples/10.8 struct function 2.cpp
@@ -0,0 +1,84 @@
+#include "stdafx.h"
+#include <iostream>
+
+//библиотека <string> переопределяет поток вывода cout 
+// для типа данных string
+#include <string>
+
+using namespace std;
+
+/*
+Создать структуру данных, содержащую информацию
+о учениках группы со следующими полями: 
+класс, фамилия, пол, оценки (10 шт.)
+Написать и протестировать функции:
+- ввода данных;
+- вывода данных;
+- расчета среднего оценки для каждого ученика;
+- определения отличников из числа учеников (оценок "5" > 80%).
+*/
+
+struct student{
+	string t_class;
+	string second_name;   //char[20]
+	bool gender;
+	int grade[5];
+};
+
+//функция вывода в консоль
+void print(student* p)		//int*  string* - адресный тип данных
+{
+	cout << p->t_class  << endl;
+	cout << p->second_name << endl;
+	if (p->gender == 1)
+		cout << "Man" << endl;
+	if (p->gender == 0)
+		cout << "Woman" << endl;
+	for (int i = 0; i < 5; i++)
+		cout << p->grade[i] << ' ';
+	cout << endl;
+}
+
+//функция ввода 
+void input(student* p)
+{
+	cout << "Enter class:" << endl;
+	cin >> p->t_class;
+	cout << "Enter second name:" << endl;
+	cin >> p->second_name;
+	cout << "Enter gender (1 - woman or 0 - man):" << endl;
+	cin >> p->gender;
+	int n = sizeof(p->grade)/sizeof(int);
+	cout << "Enter " << n << " grade:" << endl;
+	for (int i = 0; i < n; i++)
+		cin >> p->grade[i];
+	cout << "Input succefully completed!" << endl;
+}
+
+//функция расчета средней оценки
+void average(student *p)
+{
+
+}
+
+//функция определения отличников
+void excellent_student(student *p)
+{
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	student first = { "10V", "Tagilov", 1, {5,3,4,2,5} };
+	student second;
+
+	input(&second);
+
+	//student* adr = &first;
+	print(&first);
+	print(&second);  //в функцию идет передача адреса
+
+	system("pause");
+	return 0;
+}
+
