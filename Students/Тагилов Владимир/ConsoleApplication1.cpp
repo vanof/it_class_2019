6172a40a6652e43c35714e5fcb08a4b5cc638f53
vtagilov <44909454+vtagilov@users.noreply.github.com>
2020-09-14 13:26:34 +0300

    Create ConsoleApplication1.cpp

---
+++ Students/Тагилов Владимир/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream file_input("C:\\Users\\Sch1561-it-9\\Documents\\tagilov\\input.txt");
+    ofstream file_output("C:\\Users\\Sch1561-it-9\\Documents\\tagilov\\output.txt");
+    string s;
+    int k = 1;
+    int m = 1;
+    while (getline(file_input, s))
+    {
+        
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == ' ')
+            {
+                if (s[i] == ' ' and s[i + 1] == ' ')
+                {
+                    s.erase(i,1);
+                    i--;
+                }
+            }
+        }
+        file_output << s << endl;
+        cout << s.length() << ":" << s << endl;
+    }
+    file_input.close();
+    file_output.close();
+
+
+}
