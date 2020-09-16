3c0f3ad6bed7def32f5c76a548d54b0b9dc7a421
byrjat <71252378+byrjat@users.noreply.github.com>
2020-09-16 09:51:49 +0300

    ssss

--- Students/Дымченко Владимир/File_InOut.cpp
+++ Students/Дымченко Владимир/File_InOut.cpp
@@ -1,7 +1,4 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include<string>
 
---
+++ Students/Дымченко Владимир/Задание 24.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream fin("C:\\Users\\Sch1561-it-3\\Documents\\GitHub\\it_class_2019\\Tasks\\24.txt");
+
+    string s;
+    int k = 1;
+    int m = 1;
+   
+    // если файл состоит из одной строки
+    while (fin)
+
+    {
+        fin >> s;
+    }
+    cout << s.length() << endl;
+           
+            
+    for (int i = 1; i < s.length(); i++)  //проходим поэлементно всю строку
+
+    {
+
+        if (s[i] != s[i - 1])
+
+        {
+            k += 1;
+            m = max(k, m);
+        }
+
+        else
+        {
+            k = 1;
+            
+        }
+
+    }
+
+    cout << m;
+}
