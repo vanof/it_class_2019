ab403ab3dc98635fab9f7c08e42430fa91ced8dd
byrjat <71252378+byrjat@users.noreply.github.com>
2020-09-16 09:52:09 +0300

    asadghjkl

---
+++ Students/Тагилов Владимир/24 задание.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream file_input("C:\\Users\\Sch1561-it-9\\Documents\\GitHub\\it_class_2019\\Tasks\\24.txt");
+    string s;
+    int k = 0;
+    int m = 1;
+
+    while (file_input)
+    {
+        file_input >> s;
+        
+    }
+    cout << s.length() << endl;
+    
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (s[i] != s[i - 1])
+        {
+            k = k + 1;
+            m = max(k, m);
+        }
+        else
+        {
+            k = 1;
+        }
+    }
+    cout << m;
+    
+    /*
+    while (getline(file_input, s))
+    {
+        cout << s << endl;
+    }
+    file_input.close();
+    */
+
+}
