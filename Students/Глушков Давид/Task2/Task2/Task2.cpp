644e04b9b21270abea979de34fd3b90c5fe653ba
david_apriori <48278999+apriori-0@users.noreply.github.com>
2020-09-23 09:49:04 +0300

    я зделалъ.

---
+++ Students/Глушков Давид/Task2/Task2/Task2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <windows.h>
+#include <clocale>
+
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(0, ".1251");
+    string s;
+    string x = "";
+    ifstream file_input("C:\\Users\\Sch1561-it-4\\Documents\\GitHub\\it_class_2019\\Tasks\\2_1.txt");
+    ofstream file_output("C:\\Users\\Sch1561-it-4\\Documents\\GitHub\\it_class_2019\\Students\\Глушков Давид\\Task2\\output.txt");
+    while (getline(file_input, s)) {
+        cout << s << endl; 
+        break;
+    }   
+    file_output << s << endl;
+    file_output.close();
+    cout << x;
+}
---
+++ Students/Глушков Давид/Task3/Task3/Task3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(0, ".1251");
+    string s;
+    string x = "";
+    string y = "";
+    ifstream file_input("C:\\Users\\Sch1561-it-4\\Documents\\GitHub\\it_class_2019\\Tasks\\2_1.txt");
+    ofstream file_output("C:\\Users\\Sch1561-it-4\\Documents\\GitHub\\it_class_2019\\Students\\Глушков Давид\\Task2\\output.txt");
+    int i = 0;
+    while (getline(file_input, s)) {
+        if (i == 0) {
+            x += s;
+            cout << s << endl;
+        }
+        if (i == 1) {
+            y += s;
+            cout << s << endl;
+        }
+        i++;
+    }
+    file_output << y << endl << x;
+    file_output.close();
+    cout << endl << y << endl << x;
+}
