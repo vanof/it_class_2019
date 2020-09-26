8ba8ba583cc35d704b896654d6e3b35a064a1943
DmitriiZimenkov <62676259+DmitriiZimenkov@users.noreply.github.com>
2020-09-27 00:21:28 +0300

    Create максимальное количество одинаковых симолов идущих подряд .cpp
    аааааааааааааааааааааааааааааа

---
+++ Students/Зименков Дмитрий/максимальное количество одинаковых симолов идущих подряд .cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a = 0, b = 0;
+	string s;
+	ifstream fin("A:\\проекты\\Проект 1\\Проект 1\\4_1.txt");
+	//ofstream fout("A:\\проекты\\Проект 1\\Проект 1\\output.txt");
+	while (fin)
+	{
+		fin >> s;
+	}
+	/*for (int i = 1; i < s.length(); i++)
+	{
+		if (s[i] == s[i-1])
+		{
+			a = a + 1;
+		}
+		else
+		{
+			b = a;
+		}
+		if (b < a)
+		{
+			b = a;
+		}
+	}*/
+	// выводит общее количество пар
+	for (int i = 1; i < s.length(); i++)
+	{
+		if (s[i] == s[i - 1])
+		{
+			a += 1;
+			b = max(a, b);
+		}
+		else
+		{
+			a = 1;
+		}
+
+	}
+	cout << "МАКСИМАЛЬНОЕ КОЛИЧЕСТВО ОДИНАКОВЫХ СИМВОЛОВ ИДУЩИХ ПОДРЯД В ЭТОМ ЧЕРТОВО МАССИВЕ -------> " << b;
+	fin.close();
+	//fout.close();
+
+}
